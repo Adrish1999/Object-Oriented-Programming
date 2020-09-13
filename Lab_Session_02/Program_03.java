a3eefb5906ff6e3c00aa4db62d974c22fe0f4752
Adrish Bose <adrishbose198@gmail.com>
2020-09-13 13:51:28 +0530

    Create Program 3

--- Lab_Session_02/Program_03.java
+++ Lab_Session_02/Program_03.java
@@ -1,2 +1,32 @@
 //Java Program to illustrate anonymous object
 
+import java.io.*;
+
+class Factorial
+{
+    int num;
+    Factorial(int num)
+    {
+        this.num = num;
+    }
+    public int Calculate()
+    {
+        int p = 1;
+        for(int i = 1; i <= num ; i++)
+        {
+            p = p*i;
+        }
+        return p;
+    }
+}
+
+class Program_03
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a number");
+        int num = Integer.parseInt(br.readLine());
+        System.out.println("Factorial of "+num+" is "+new Factorial(num).Calculate()); // Anonymous object
+    }
+}
