bdd5b8a7968a2a2103749b8eb062da4584e86224
Adrish Bose <adrishbose198@gmail.com>
2020-11-10 15:19:35 +0530

    Lab Session 7 done

--- Lab_Session_07_Command_Line/Program_01.java
+++ Lab_Session_07_Command_Line/Program_01.java
@@ -1,4 +1,4 @@
-//Program to find out maximum and minimum out of 5 integers using command line argument
+//Program to find out maximum and minimum out of n integers using command line argument
 
 import java.io.*;
 
@@ -6,9 +6,11 @@ class Program_01
 {
     public static void main(String args[])
     {
-        int max = Integer.parseInt(args[0]);
-        int min = Integer.parseInt(args[0]);
-        for(int i = 1; i <= 4; i++)
+        int n = Integer.parseInt(args[0]);
+        int max = Integer.parseInt(args[1]);
+        int min = Integer.parseInt(args[1]);
+        System.out.println("Value of n entered by the user is : "+n);
+        for(int i = 2; i < n; i++)
         {
             if(Integer.parseInt(args[i]) > max)
             {
@@ -18,6 +20,11 @@ class Program_01
             {
                 min = Integer.parseInt(args[i]);
             }
+        }
+        System.out.println(n+" numbers entered by the user are :");
+        for(int i = 1; i < n; i++)
+        {
+            System.out.println(Integer.parseInt(args[i]));
         }
         System.out.println("Maximum number is : "+max);
         System.out.println("Minimum number is : "+min);
