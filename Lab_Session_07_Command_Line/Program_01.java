d5c1fb2773dbf5d75c634ff7fd6bcd971c9e660f
Adrish Bose <adrishbose198@gmail.com>
2020-11-10 14:28:56 +0530

    Lab Session 7 started

--- Lab_Session_07_Command_Line/Program_01.java
+++ Lab_Session_07_Command_Line/Program_01.java
@@ -1,4 +1,25 @@
-//Program to find out maximum and minimum out of 5 numbers
+//Program to find out maximum and minimum out of 5 integers using command line argument
 
 import java.io.*;
 
+class Program_01
+{
+    public static void main(String args[])
+    {
+        int max = Integer.parseInt(args[0]);
+        int min = Integer.parseInt(args[0]);
+        for(int i = 1; i <= 4; i++)
+        {
+            if(Integer.parseInt(args[i]) > max)
+            {
+                max = Integer.parseInt(args[i]);
+            }
+            if(Integer.parseInt(args[i]) < min)
+            {
+                min = Integer.parseInt(args[i]);
+            }
+        }
+        System.out.println("Maximum number is : "+max);
+        System.out.println("Minimum number is : "+min);
+    }
+}
