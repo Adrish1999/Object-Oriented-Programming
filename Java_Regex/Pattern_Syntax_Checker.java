879ad39514853b6e04c50ef99a6288940addc192
Adrish <54068755+Adrish1999@users.noreply.github.com>
2020-12-21 22:52:51 +0530

    Create Pattern_Syntax_Checker.java

--- Java_Regex/Pattern_Syntax_Checker.java
+++ Java_Regex/Pattern_Syntax_Checker.java
@@ -3,26 +3,26 @@ import java.util.regex.*;
 import java.util.regex.PatternSyntaxException;
 
 
-public class Solution
+public class Pattern_Syntax_Checker
 {
 	public static void main(String[] args)
-  {
+  	{
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
     
 		while(in.hasNextLine())
-    {
+    		{
 			String pattern = in.nextLine();
           	
-            try
-            {
-                Pattern pat = Pattern.compile(pattern);
-                System.out.println("Valid");
-            }
-            catch(PatternSyntaxException e)
-            {
-                System.out.println("Invalid");
-            }
+            		try
+            		{
+                		Pattern pat = Pattern.compile(pattern);
+                		System.out.println("Valid");
+            		}
+            		catch(PatternSyntaxException e)
+            		{
+               			 System.out.println("Invalid");
+            		}
 		}
 	}
 }
