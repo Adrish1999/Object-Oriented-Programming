913ef5d5921cdbe04b6f94116e4192b5185f893e
Adrish <54068755+Adrish1999@users.noreply.github.com>
2020-12-21 22:50:48 +0530

    Create Pattern_Syntax_Checker.java

---
+++ Java_Regex/Pattern_Syntax_Checker.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.*;
+import java.util.regex.PatternSyntaxException;
+
+
+public class Solution
+{
+	public static void main(String[] args)
+  {
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+    
+		while(in.hasNextLine())
+    {
+			String pattern = in.nextLine();
+          	
+            try
+            {
+                Pattern pat = Pattern.compile(pattern);
+                System.out.println("Valid");
+            }
+            catch(PatternSyntaxException e)
+            {
+                System.out.println("Invalid");
+            }
+		}
+	}
+}
